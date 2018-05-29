56b523e406398ba5ad0b8fd39aaa934502ce8641
dumok <nsh330@gmail.com>
2018-05-29 23:32:14 +0900

    Ch3, Add equality

--- dollar/dollar.py
+++ dollar/dollar.py
@@ -1,9 +1,18 @@
 
 class Dollar(object):
-	"""docstring for Dollar"""
-	def __init__(self, amount):
-		super(Dollar, self).__init__()
-		self.amount = amount
-	
-	def times(self, multiplier):
-		return Dollar(self.amount * multiplier)
+    """docstring for Dollar"""
+    def __init__(self, amount):
+        super(Dollar, self).__init__()
+        self.amount = amount
+
+    def times(self, multiplier):
+        return Dollar(self.amount * multiplier)
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.__dict__ == other.__dict__
+        else:
+            return False
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
--- tests/test_money.py
+++ tests/test_money.py
@@ -1,9 +1,15 @@
 from dollar import dollar
 
-def test_multiplication():
-	d = dollar.Dollar(5)
-	product = d.times(2)
-	assert 10 == product.amount
 
-	product = d.times(3)
-	assert 15 == product.amount
+def test_multiplication():
+    d = dollar.Dollar(5)
+    product = d.times(2)
+    assert 10 == product.amount
+
+    product = d.times(3)
+    assert 15 == product.amount
+
+
+def test_equality():
+    assert dollar.Dollar(5) == dollar.Dollar(5)
+    assert dollar.Dollar(5) != dollar.Dollar(6)
